a362eb14e488f0571bc36be687527e83fc073018
valenzsa <valenzsa418@gmail.com>
2022-04-13 22:37:05 -0400

    Update index.js file to use createRoot for react 18

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+// createRoot(document.getElementById('root')).render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>
+// );
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
