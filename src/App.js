be4d2c46c4c8e2a8f81e1440d913cdb880e63ae7
valenzsa <valenzsa418@gmail.com>
2022-04-13 22:26:26 -0400

    Create sample counter application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Counter: { counter }
+
+      <button onClick={() => {setCounter(counter + 1)}}>Increment</button>
+      <button onClick={() => {setCounter(counter - 1)}}>Decrement</button>
     </div>
   );
 }
